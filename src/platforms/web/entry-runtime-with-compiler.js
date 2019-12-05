484b4b2c40d8811d647adc3c98f13c01f4f07de7
chenjinrui <chenjinrui@moerlong.com>
2019-12-05 09:28:22 +0800

    feat: 从构建入口 copy vue build progress

---
+++ src/platforms/web/entry-runtime-with-compiler.js
@@ -0,0 +1,8 @@
+/* @flow */
+
+import config from 'core/config'
+import { warn, cached } from 'core/util/index'
+import { mark, measure } from 'core/util/perf'
+
+import Vue from './runtime/index'
+
