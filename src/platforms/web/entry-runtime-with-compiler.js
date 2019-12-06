4fae302837f1f490ef4a9270e139c55f2a1ab7e7
chenjinrui <chenjinrui@moerlong.com>
2019-12-06 14:46:19 +0800

    feat:copy vue shared utils code word by word

--- src/platforms/web/entry-runtime-with-compiler.js
+++ src/platforms/web/entry-runtime-with-compiler.js
@@ -5,4 +5,12 @@ import { warn, cached } from 'core/util/index'
 import { mark, measure } from 'core/util/perf'
 
 import Vue from './runtime/index'
+import { query } from './util/index'
+import { query } from './util/index'
 
+Vue.prototype.$mount = function (
+  el?: string | Element,
+  hydrating?: boolean
+): Component {
+  el = el && query(el)
+}
